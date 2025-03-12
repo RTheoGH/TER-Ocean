0c467eaf7a0eaa288a8fa4191a9d58d8a6938a32
Sloth-King <andrew.mansour24@gmail.com>
2025-03-12 13:54:07 +0100

    created lean mapping compute shader

---
+++ addons/tessarakkt.oceanfft/shaders/ComputeLEAN.glsl
@@ -0,0 +1,34 @@
+#[compute]
+#version 460 core
+
+#define WORK_GROUP_DIM 256
+#define PI 3.14159265358979323846
+
+layout(local_size_x = WORK_GROUP_DIM) in;
+
+
+//input/output
+
+//normal map -> rgba cuz vec4
+layout(set = 0 ,binding = 0 , rgba32f) uniform image2D u_normals_input;
+
+//M and B (maybe change from sampler to image but idk LeWhatdatmean yet)
+layout(set = 0 , binding = 1 , rg32f) uniform image2D u_B_output; //rg cuz xy
+layout(set = 0 , binding = 2 , rg32f) uniform image2D u_M_output; //rgv cuz xyz
+
+void main(){
+    //gets the coordinates of a pixel from our texture
+    ivec2 pixel_coord = ivec2(gl_GlobalInvocationID.xy);
+    vec3 normal = imageLoad(u_normals_input, pixel_coord).rgb;
+
+    if (normal.z == 0.0) normal.z = 0.00001;
+    vec2 B = normal.xy / normal.z;
+    vec3 M = vec3(B.x * B.x, B.y * B.y, B.x * B.y);
+
+    imageStore(u_B_output, pixel_coord, vec4(B, 0.0, 0.0));
+    imageStore(u_M_output, pixel_coord, vec4(M, 0.0));
+}
+
+
+
+
